bc6bd23cd5355ac9151f21022deb77ea3e225e17
unknown <jamilafzal255@gmail.com>
2024-10-22 19:11:41 +0500

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+const Home = lazy(() => import('./pages/home/index'))
+const Login = lazy(() => import('./pages/login'))
+const Chat = lazy(() => import('./pages/chats/index'))
+const Group = lazy(() => import('./pages/groups/index'))
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-              <Route index element={<p>helloo</p>} />
-        <Route />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/group" element={<Group />} />
         <Route />
       </Routes>
     </BrowserRouter>
